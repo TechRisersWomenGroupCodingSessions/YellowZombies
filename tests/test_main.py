a15d535c65a3f16c5facf79cd4cab9e012c95346
becky <becky.zhao@outlook.com>
2021-04-14 19:01:25 +0100

    setup

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,6 @@
 # write tests here, don't forget to import what you need and
 # info: https://docs.pytest.org/en/latest/getting-started.html#group-multiple-tests-in-a-class
+from main.main import Survivors
+class TestZombies:
+
+    def test_printsurvivor(self):
