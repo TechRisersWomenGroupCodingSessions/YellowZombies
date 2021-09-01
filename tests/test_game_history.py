1003e944b7b75eafbaa4284d5400e705cb33b805
becky <becky.zhao@outlook.com>
2021-09-01 18:35:52 +0100

    tried to mock

--- tests/test_game_history.py
+++ tests/test_game_history.py
@@ -1,17 +1,25 @@
+import datetime
+
 from main.game import Game
 from main.survivor import Survivor
+from unittest.mock import MagicMock, patch
 
+#future idea: mock datetime
 
 class TestGameHistory:
-
+    #@patch("datetime.datetime.now", return_value=MagicMock())
     def test_game_history_begins_by_recording_time(self):
+        #mock_datetime_now.return_value = datetime(2001, 1, 1)
+        #datetime_mock = MagicMock(wrap=datetime.datetime)
+        #mock_datetime_now.return_value = datetime.datetime(2020, 3, 11, 0, 0, 0)
+        #monkeypatch.setattr(datetime, "datetime", datetime_mock)
         game = Game()
         assert len(game.history) == 1
         first_game_event = game.history[0]
         assert first_game_event.action == "started game"
-        assert first_game_event.timestamp != None
+        assert first_game_event.timestamp is not None
 
-    def test_game_history_survior_added(self):
+    def test_game_history_survivor_added(self):
         game = Game()
         survivor = Survivor("Becky")
         game.add_survivor(survivor)
